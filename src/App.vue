42f4c987dad49cc5ce4c6f03de85319f437eed19
ivan-suarez <ivansuarezcast@gmail.com>
2022-07-12 19:48:01 +0000

    Add axios support

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,28 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
+  <p>{{message}}</p>
+    <button v-on:click="hello()" >Log in</button>
   <HelloWorld msg="Welcome to Your Vue.js App"/>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import axios from 'axios'
 
 export default {
   name: 'App',
+  data: ()=>{
+      return{
+          message:"hi"
+      }
+  },
   components: {
     HelloWorld
+  }, methods: {
+      async hello(){
+          console.log("helllo")
+          this.message = await axios.get("https://8081-cs-515518291237-default.cs-europe-west1-iuzs.cloudshell.dev");
+      }
   }
 }
 </script>
