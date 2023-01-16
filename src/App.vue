6f8e4c04e9f4aa6579625d7594191e35f64173b8
ivan-suarez <ivansuarezcast@gmail.com>
2023-01-16 18:09:25 +0100

    Added bookmark route

--- src/App.vue
+++ src/App.vue
@@ -1,43 +1,11 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <p>{{message}}</p>
-    <button v-on:click="hello()" >Log in</button>
-    <input type="text" v-model="callbackurl">
-    <button v-on:click="getBookmarks()" >Get bookmarks</button>
-  <p>{{tweets}}</p>
+  <nav>
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link> |
+    <router-link to="/bookmarks">Bookmarks</router-link>
+  </nav>
+  <router-view/>
 </template>
-
-<script>
-import axios from 'axios'
-
-export default {
-  name: 'App',
-  data: ()=>{
-      return{
-          message:"hi",
-          tweets:"",
-          callbackurl:""
-      }
-  },methods: {
-      async hello(){
-          console.log("helllo")
-          const response = await axios.get("http://localhost:8081/generateUrl");
-          this.message = response.data;
-          // this.message = "hello";
-      },
-      async getBookmarks(){
-          console.log(this.callbackurl);
-          const response = await axios.post("http://localhost:8081/getBookmarks", {url: this.callbackurl});
-          this.tweets = response.data;
-          //this.message = response.data;
-          // this.message = "hello";
-      },
-      onInput(e){
-        this.callbackurl = e.target.value;
-      }
-  }
-}
-</script>
 
 <style>
 #app {
@@ -46,6 +14,18 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+nav {
+  padding: 30px;
+}
+
+nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+nav a.router-link-exact-active {
+  color: #42b983;
 }
 </style>
